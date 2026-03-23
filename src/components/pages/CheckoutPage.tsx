a811c060fe6d6eba9f60c4c598f6e2d08869be32
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2026-03-23 16:25:28 +0300

    use refearnapp

--- src/components/pages/CheckoutPage.tsx
+++ src/components/pages/CheckoutPage.tsx
@@ -35,7 +35,7 @@ export default function CheckoutPage({
 
   // Fetch promos on load
   useEffect(() => {
-    initRefearnapp("https://origin.refearnapp.com");
+    initRefearnapp("https://refearnapp.com");
     getAllAvailablePromos().then(setAvailablePromos);
   }, []);
   async function handleManualTrack() {
